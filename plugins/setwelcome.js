180379e3a3822ab224d74e7178260f651d18f0e8
TengkuShafiq <132145610+TengkuShafiq@users.noreply.github.com>
2023-05-21 02:31:32 +0800

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -1,12 +1,21 @@
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
-  if (text) {
-    if (isROwner) global.conn.welcome = text
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  await conn.groupUpdateSubject(m.chat, `${args.join(" ")}`);
+  m.reply('Sukses set welcome group')
+if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     global.db.data.chats[m.chat].sWelcome = text
     m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
-  } else throw 'Teksnya mana?'
+  } else throw 'Teksnya mana?
 }
-handler.help = ['setwelcome <teks>']
-handler.tags = ['owner', 'group']
+handler.help = ['setwelcome <text>']
+handler.tags = ['group']
 handler.command = /^setwelcome$/i
-module.exports = handler
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.register = false
+handler.admin = true
+handler.botAdmin = true
+export default handler
